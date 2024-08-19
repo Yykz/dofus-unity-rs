c290219a628a05d21bd0c0db12ae631a4521998b
Yikz <yykz@protonmail.com>
2024-08-19 19:05:03 +0200

    refactor CreateOnWriteFile, renaming it ProtoFileBuffer

--- proto-gen/src/generator.rs
+++ proto-gen/src/generator.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufWriter};
+use std::io::{self, BufWriter, Write};
 use std::path::{Path, PathBuf};
 
 use pest::Parser;
@@ -6,49 +6,37 @@ use pest::Parser;
 use crate::parser_items::{File, FileContent};
 use crate::{CSharpParser, ProtoEntity, Rule};
 
-struct CreateOnWriteFile<'a> {
-    path: &'a Path,
-    package_prefix: &'a str,
-    maybe_file: Option<BufWriter<std::fs::File>>,
+#[derive(Debug, Default)]
+struct ProtoFileBuffer {
+    content: Vec<u8>,
 }
 
-impl<'a> CreateOnWriteFile<'a> {
-    fn new(path: &'a Path, s: &'a str) -> Self {
-        Self {
-            path,
-            maybe_file: None,
-            package_prefix: s
+impl ProtoFileBuffer {
+    pub fn write_to_file(&self, path: &Path, package: &str) -> io::Result<()> {
+        if self.content.is_empty() {
+            return Ok(());
         }
+        let mut file = std::fs::File::create(path)?;
+        writeln!(file, "syntax = \"proto3\";\n\npackage {};\n", package)?;
+        file.write_all(&self.content)?;
+        Ok(())
     }
 }
 
-impl<'a> io::Write for CreateOnWriteFile<'a> {
+impl io::Write for ProtoFileBuffer {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        match self.maybe_file {
-            Some(ref mut bufwriter) => bufwriter.write(buf),
-            None => {
-                let file = std::fs::File::create(self.path)?;
-                let mut bufwriter = BufWriter::new(file);
-                writeln!(bufwriter, "syntax = \"proto3\";\n\npackage {}.{};\n", self.package_prefix, self.path.file_stem().unwrap().to_str().unwrap())?;
-                let r = bufwriter.write(buf);
-                self.maybe_file = Some(bufwriter);
-                r
-            }
-        }
+        self.content.write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        match self.maybe_file {
-            Some(ref mut bufwriter) => bufwriter.flush(),
-            None => Ok(()),
-        }
+        self.content.flush()
     }
 }
 
 pub struct Generator {
     source: PathBuf,
     outdir: PathBuf,
-    package_prefix: String
+    package_prefix: String,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -57,20 +45,26 @@ pub enum Error {
     FailedToParse(#[from] pest::error::Error<Rule>),
     #[error("failed to read file {0} {1}")]
     FailedToReadFile(PathBuf, io::Error),
-    #[error("failed to write to file {0}")]
+    #[error("failed to write to proto buffer {0}")]
     FailedToWriteProto(io::Error),
     #[error("failed to read dir {0} {1}")]
     FailedToReadDir(PathBuf, io::Error),
+    #[error("failed to write to file {0} {1}")]
+    FailedToWriteFile(PathBuf, io::Error),
 }
 
 pub type Result = std::result::Result<(), Error>;
 
 impl Generator {
-    pub fn from_source<P: AsRef<Path>, O: Into<PathBuf>, S: Into<String>>(source: P, outdir: O, package_prefix: S) -> Result {
+    pub fn from_source<P: AsRef<Path>, O: Into<PathBuf>, S: Into<String>>(
+        source: P,
+        outdir: O,
+        package_prefix: S,
+    ) -> Result {
         Self {
             source: source.as_ref().into(),
             outdir: outdir.into(),
-            package_prefix: package_prefix.into()
+            package_prefix: package_prefix.into(),
         }
         .process_dir_recu(source.as_ref())
     }
@@ -83,26 +77,34 @@ impl Generator {
             affix.to_str().unwrap().to_lowercase().replace("/", ".")
         ));
 
-        let mut file = CreateOnWriteFile::new(&path_outfile, &self.package_prefix);
-
+        let mut protofile = ProtoFileBuffer::default();
+        
         for entry in std::fs::read_dir(path).map_err(|e| Error::FailedToReadDir(path.into(), e))? {
             let entry = entry.map_err(|e| Error::FailedToReadDir(path.into(), e))?;
             let path = entry.path();
 
             match path.is_file() {
                 true => {
-                    Self::process_file(&path, &mut file)?;
+                    Self::process_file(&path, &mut protofile)?;
                 }
                 false => {
                     self.process_dir_recu(&path)?;
                 }
             }
         }
-        Ok(())
+        let package_file = path_outfile
+            .file_stem()
+            .and_then(|stem| stem.to_str())
+            .unwrap();
+        let package = format!("{}.{}", self.package_prefix, package_file);
+        protofile
+            .write_to_file(&path_outfile, &package)
+            .map_err(|e| Error::FailedToWriteFile(path_outfile.into(), e))
     }
 
     fn process_file<W: io::Write>(path: &Path, mut out: W) -> Result {
-        let content = std::fs::read_to_string(path).map_err(|e| Error::FailedToReadFile(path.into(), e))?;
+        let content =
+            std::fs::read_to_string(path).map_err(|e| Error::FailedToReadFile(path.into(), e))?;
         let mut parsed = CSharpParser::parse(Rule::file, &content[3..])?;
         let file_pair = parsed.next().unwrap();
         let parsed_file = File::try_from(file_pair).unwrap();
