4366aa023596cb32afb700df8e77719b729cc9ad
Yikz <yykz@protonmail.com>
2024-08-19 12:53:56 +0200

    change protos dir

--- proto-gen/src/main.rs
+++ proto-gen/src/main.rs
@@ -2,6 +2,6 @@ use std::io;
 use proto_gen::Generator;
 
 fn main() -> io::Result<()> {
-    Generator::from_source("./Ankama.Dofus.Protocol.Game/", "protos/".into());
+    Generator::from_source("./Ankama.Dofus.Protocol.Game/", "game_protos/".into());
     Ok(())
 }
