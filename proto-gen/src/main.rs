8c14253c866b36155291a48fc94ac0001e22725c
Yikz <yykz@protonmail.com>
2024-09-07 17:44:38 +0200

    add dump.cs to ignore

--- proto-gen/src/main.rs
+++ proto-gen/src/main.rs
@@ -1,7 +1,7 @@
 use proto_gen::{ParsedFile, ProtoPackages};
 
 fn main() {
-    let file = ParsedFile::try_from_dump(include_str!("../dump.cs")).unwrap();
+    let file = ParsedFile::try_from_dump(include_str!("../../dump.cs")).unwrap();
     let packages = ProtoPackages::from_parsed(file, "Com.Ankama.Dofus.Server");
     packages.write_to_dir("./protos/");
 }
